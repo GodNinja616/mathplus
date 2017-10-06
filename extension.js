1a9982e4ca445bc2b9d5a39c00967c4e3381cbc2
GodNinja616 <otavioapv@hotmail.com>
2017-10-06 08:41:55 -0300

    aAaAAaaaaa

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@
         }
     }
 
-    ext.modu = function(val1, val2) {
+    ext.modu = function(val1,val2) {
         return (val % val2);
     }
 
@@ -27,7 +27,7 @@
         blocks: [
             ['r', 'Map- %n : %n : %n : %n : %n', 'map', 20, 1,0,5,20],
             ['r', 'Value- %m.type', 'val', "PI"],
-            ['R', '%n % %n', 'modu', 3, 5],
+            ['R', 'Modulus- %n % %n', 'modu', 3, 5],
         ],
         menus: {
             type: ['PI', 'PHI', '2PI']
