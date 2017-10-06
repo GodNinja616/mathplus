3a790c30bb9ad00d677b7fe813ed6254f100663a
GodNinja616 <otavioapv@hotmail.com>
2017-10-06 08:37:18 -0300

    updating

--- extension.js
+++ extension.js
@@ -27,7 +27,7 @@
         blocks: [
             ['r', 'Map- %n : %n : %n : %n : %n', 'map', 20, 1,0,5,20],
             ['r', 'Value- %m.type', 'val', "PI"],
-            ['r', '%n % %n', 'modu'],
+            ['r', '%n % %n', 'modu', 3, 5],
         ],
         menus: {
             type: ['PI', 'PHI', '2PI']
