522564a2e522d293160bce5de6103c7c646957d9
GodNinja616 <otavioapv@hotmail.com>
2017-10-06 09:01:52 -0300

    why github why bug every new commit

---
+++ main.js
@@ -0,0 +1,38 @@
+(function(ext) {
+  ext._shutdown = function() {};
+
+  ext._getStatus = function() {
+      return {status: 2, msg: 'Ready'};
+  };
+
+  ext.map = function(value,istart,istop,ostart,ostop) {
+      return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+  };
+
+  ext.val = function(type) {
+      if (type == "PI") {
+          return Math.PI;
+      } else if (type == "PHI") {
+          return Math.PI/2;
+      } else if (type == "2PI") {
+          return Math.PI*2;
+      }
+  }
+
+  ext.modu = function(val1,val2) {
+      return (val % val2);
+  }
+
+  var descriptor = {
+      blocks: [
+          ['r', 'Map- %n : %n : %n : %n : %n', 'map', 20, 1,0,5,20],
+          ['r', 'Value- %m.type', 'val', "PI"],
+          ['r', 'Modulus- %n % %n', 'modu', 3, 5],
+      ],
+      menus: {
+          type: ['PI', 'PHI', '2PI']
+      }
+  };
+
+  ScratchExtensions.register('Math+', descriptor, ext);
+})({});
